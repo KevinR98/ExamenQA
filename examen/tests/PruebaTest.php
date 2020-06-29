436ce77ad2e420e2da0039a71ec982253b12ebc7
Kevin <kevin.rojas277@outlook.es>
2020-06-28 23:17:42 -0600

    Correccion de bug.

--- examen/tests/PruebaTest.php
+++ examen/tests/PruebaTest.php
@@ -8,9 +8,6 @@ class PruebaTest extends TestCase
     public function testSameInt()
     {
         require_once(ROOT.'/src/Prueba.php');
-        //require_once('/Users/kevinrojas/Desktop/Examen QA I/ExamenQA/examen/src/Prueba.php');
-
-
         $objeto = new Prueba();
         $this->assertEquals(
             100,
